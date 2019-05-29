e2db8ea5ed94096d83148afe08fbae07649c4694
AmmarAlkhooly98 <ammaralkhooly1@gmail.com>
2019-05-29 21:51:43 +0300

    modified the restaurant profile

--- server.js
+++ server.js
@@ -168,7 +168,7 @@ app.post('/login', function(req, res) {
 	var username = req.body.UserName;
 	var password = req.body.Password;
 	//const Find = 'select'`+re+`;
-	const user = "SELECT * From usersInfo Where Name =  '" + username + "' and Password = '" + password + "'";
+	const user = "SELECT * From usersInfo Where Email =  '" + username + "' and Password = '" + password + "'";
 
 	connection.query(user, (err, result) => {
 		if (result) {
