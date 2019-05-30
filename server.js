208e3435319da4bbca70013213200593572f3d7b
areejobaid94 <areej.obaid@yahoo.com>
2019-05-30 14:59:15 +0300

    final work

--- server.js
+++ server.js
@@ -93,64 +93,64 @@ app.use(express.static('Angular'));
 // 	req.session.errors = null;
 // });
 
-app.post('/registered', function(req, res, next) {
-	const User = req.body.price;
-	req.check('UserName', 'Invald Email Plese Try Another One').isEmail();
-	req.check('Password', 'The Password Should be Numbers').isNumeric().isLength({ min: 8 });
-	var err = req.validationErrors();
+// `app.post('/registered', function(req, res, next) {
+// 	const User = req.body.price;
+// 	// req.check('UserName', 'Invald Email Plese Try Another One').isEmail();
+// 	// req.check('Password', 'The Password Should be Numbers').isNumeric().isLength({ min: 8 });
+// 	var err = req.validationErrors();
 
-	let newRestaurant = {
-		Name: req.body.UserName,
-		password: req.body.Password,
-		location: req.body.Location,
-		phonenumber: req.body.PhoneNumber,
-		TheRestaurant: req.body.Restaurant,
-		MealsandPrice: req.body.PriceandMeal
-	};
-	const Check = `SELECT * From userInfo Where Name = ${req.body.UserName}`;
-	const added = 'INSERT INTO usersInfo SET ?';
+// 	let newRestaurant = {
+// 		Name: req.body.UserName,
+// 		password: req.body.Password,
+// 		location: req.body.Location,
+// 		phonenumber: req.body.PhoneNumber,
+// 		TheRestaurant: req.body.Restaurant,
+// 		MealsandPrice: req.body.PriceandMeal
+// 	};
+// 	// const Check = `SELECT * From userInfo Where Name = ${req.body.UserName}`;
+// 	 const added = 'INSERT INTO usersInfo SET ?';
 
-	console.log(newRestaurant);
+// 	console.log(newRestaurant);
 
-	UsersConection.query(added, newRestaurant, (err, result) => {
-		console.log(newRestaurant);
-		if (err) throw err;
-		console.log(result);
-		console.log('User Was Added');
-	});
+// 	UsersConection.query(added, newRestaurant, (err, result) => {
+// 		console.log(newRestaurant);
+// 		if (err) throw err;
+// 		console.log(result);
+// 		console.log('User Was Added');
+// 	});
 
-	SEND EMAIL START
-	const nodemailer = require('nodemailer');
+// 	SEND EMAIL START
+// 	const nodemailer = require('nodemailer');
 
-	var transporter = nodemailer.createTransport({
-		service: 'gmail',
-		auth: {
-			user: 'tafran56@gmail.com',
-			pass: '12345678D!'
-		},
-		tls: {
-			rejectUnauthorized: false
-		}
-	});
+// 	var transporter = nodemailer.createTransport({
+// 		service: 'gmail',
+// 		auth: {
+// 			user: 'tafran56@gmail.com',
+// 			pass: '12345678D!'
+// 		},
+// 		tls: {
+// 			rejectUnauthorized: false
+// 		}
+// 	});
 
-	var mailOptions = {
-		from: 'tafran56@gmail.com',
-		to: `${req.body.UserName}`,
-		subject: 'TAFRAN.inc Registerd in TAFRAN',
-		text: `Thank You For Registerd We Will Be in Toch With You Soon`,
-		html: `<h1 style="color:#000">TAFRAN.inc</h1>
+// 	var mailOptions = {
+// 		from: 'tafran56@gmail.com',
+// 		to: `${req.body.UserName}`,
+// 		subject: 'TAFRAN.inc Registerd in TAFRAN',
+// 		text: `Thank You For Registerd We Will Be in Toch With You Soon`,
+// 		html: `<h1 style="color:#000">TAFRAN.inc</h1>
 
-    <h3 style="color:#000">Thank You For Registerd We Will Be in Toch With You Soon</h3>
-    <h1 style="color:#000">More Info About TAFRAN App</h1>
-    <h4 style="color:#000>This App Is to Make your life Easy when You order food You just Put in How much you want to eat and we will</br>
-    take care of the rest we will give you famous restaurnts and none famous restaurnt how have meals under your Budget and You will pick Your favorite</h4></br>
-    <h1 style ="color:#000">Add Your Restaurnt to our Applictaion</h1>
-    <h4 style="color:#000">Your Restaurnt will Be Exposse To thousands of people every day and You can get benefit from this to get new customers and make your busness biger.</h4></br>
-    <h1 style="color:#000">Whate the requirements to get your restarunt accsepted</h1>
-    <h4 style="color:#000">All You need to have is good restarunt and have a good customer servers and our team will come to you to check every thing and you will be accspted </h4>
-    `
-	};
-
+//     <h3 style="color:#000">Thank You For Registerd We Will Be in Toch With You Soon</h3>
+//     <h1 style="color:#000">More Info About TAFRAN App</h1>
+//     <h4 style="color:#000>This App Is to Make your life Easy when You order food You just Put in How much you want to eat and we will</br>
+//     take care of the rest we will give you famous restaurnts and none famous restaurnt how have meals under your Budget and You will pick Your favorite</h4></br>
+//     <h1 style ="color:#000">Add Your Restaurnt to our Applictaion</h1>
+//     <h4 style="color:#000">Your Restaurnt will Be Exposse To thousands of people every day and You can get benefit from this to get new customers and make your busness biger.</h4></br>
+//     <h1 style="color:#000">Whate the requirements to get your restarunt accsepted</h1>
+//     <h4 style="color:#000">All You need to have is good restarunt and have a good customer servers and our team will come to you to check every thing and you will be accspted </h4>
+//     `
+// 	};
+`
 	transporter.sendMail(mailOptions, function(error, info) {
 		if (error) {
 			console.log(error);
